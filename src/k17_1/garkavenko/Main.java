2fe43c19bfc70a16bab713b85e6bce14b5390166
krdq@ <krdq@protonmail.com>
2020-10-07 01:55:01 +0300

    Задание 2.2

--- src/k17_1/garkavenko/Main.java
+++ src/k17_1/garkavenko/Main.java
@@ -3,16 +3,14 @@ package k17_1.garkavenko;
 import java.util.Scanner;
 
 public class Main {
-    // Дана целочисленная прямоугольная матрица. Определить:
-    //1)количество столбцов, не содержащих ни одного нулевого элемента.
-    //2)сформировать одномерный массив из сумм элементов тех строк, которые содержат хотя бы один положительный элемент
+
     public static void main(String[] args) {
 
         Matrix instance = new Matrix(3);
         instance.AutoComplete();
         instance.PrintMatrix();
         System.out.println(instance.FoundCountOfColumnsWithoutZero());
-        int[] sumOfPositiveNumbers =  instance.GetSumOfPositiveNumbers();
+        int[] sumOfPositiveNumbers = instance.GetSumOfPositiveNumbers();
         Debug(sumOfPositiveNumbers);
         System.out.print("\nЗадание 2.1\n");
         Scanner in = new Scanner(System.in);
@@ -20,22 +18,53 @@ public class Main {
         String text = in.next();
         System.out.print("Ведите символ: ");
         char symbol = in.next().charAt(0);
-        System.out.println( ReplaceMiddle(text,symbol));
+        in.nextLine();
+        System.out.println(ReplaceMiddle(text, symbol));
         System.out.print("\nЗадание 2.2\n");
-    }
-    private static void Debug(int[] temp){
-        for (int el: temp) {
-            System.out.print(el+",");
+        System.out.print("Ведите текст: ");
+        text = in.nextLine();
+        System.out.print("Ведите слово для поиска: ");
+        String word = in.next();
+        String[] arr = text.split("[.?!]+");
+        for (String s : arr) {
+            if (IsWordFind(s.toLowerCase(), word))
+                System.out.println(s.trim());
         }
     }
-    public static String ReplaceMiddle(String _text, char symbol)
-    {
+
+
+    private static void Debug(int[] temp) {
+        for (int el : temp) {
+            System.out.print(el + ",");
+        }
+    }
+
+    public static String ReplaceMiddle(String _text, char symbol) {
         String reverse = new StringBuffer(_text).reverse().toString();
         StringBuilder sb = new StringBuilder(reverse);
-        if(reverse.length()%2==0)
-            sb.insert(reverse.length()/2, symbol);
+        if (reverse.length() % 2 == 0)
+            sb.insert(reverse.length() / 2, symbol);
         else
-            sb.replace(reverse.length()/2,reverse.length()/2, String.valueOf(symbol));
+            sb.replace(reverse.length() / 2, reverse.length() / 2, String.valueOf(symbol));
         return sb.toString();
     }
+
+    public static boolean IsWordFind(String s, String w) {
+        int p = 0;
+        while ((p = s.indexOf(w, p)) != -1) {
+            if ((p == 0 || !Character.isLetter(s.charAt(p - 1))) &&
+                    (p + w.length() >= s.length() || !Character.isLetter(s.charAt(p + w.length()))))
+                return true;
+            p += w.length();
+        }
+        return false;
+    }
+
+//    public static String ExUra(String _text, char symbol) {
+//        StringBuilder sb = new StringBuilder(_text);
+//        int index = sb.indexOf(String.valueOf('a'));
+//        sb.replace(index,index, String.valueOf(symbol));
+//        return sb.toString();
+//    }
+
 }
