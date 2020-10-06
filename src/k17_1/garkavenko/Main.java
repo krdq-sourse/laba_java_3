af5aed884501b98035f8112b99e82a0a8466365d
krdq@ <krdq@protonmail.com>
2020-10-07 00:59:05 +0300

    Задание 2.1

--- src/k17_1/garkavenko/Main.java
+++ src/k17_1/garkavenko/Main.java
@@ -12,10 +12,22 @@ public class Main {
         System.out.println(instance.FoundCountOfColumnsWithoutZero());
         int[] sumOfPositiveNumbers =  instance.GetSumOfPositiveNumbers();
         Debug(sumOfPositiveNumbers);
+        System.out.print("\nЗадание 2.1\n");
+        System.out.println( ReplaceMiddle("abajavaj",' '));
     }
     private static void Debug(int[] temp){
         for (int el: temp) {
             System.out.print(el+",");
         }
+    }
+    public static String ReplaceMiddle(String _text, char symbol)
+    {
+        String reverse = new StringBuffer(_text).reverse().toString();
+        StringBuilder sb = new StringBuilder(reverse);
+        if(reverse.length()%2==0)
+            sb.insert(reverse.length()/2, symbol);
+        else
+            sb.replace(reverse.length()/2,reverse.length()/2, String.valueOf(symbol));
+        return sb.toString();
     }
 }
